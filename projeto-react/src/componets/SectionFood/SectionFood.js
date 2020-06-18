779d16dabc7c4794abfda16b91ddd707bf931e57
Ilza <ilzammed2@gmail.com>
2020-06-18 16:00:42 -0300

    crido componente com funçao onClick para alternar imagens

--- projeto-react/src/componets/SectionFood/SectionFood.js
+++ projeto-react/src/componets/SectionFood/SectionFood.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import sushi from '../../img/sushi.jpeg';
+import burguer from '../../img/hamburguer.jpeg';
+import combo from '../../img/combo.jpg'
+
 
 class SectionFood extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            imagemFood: combo
+        }
+    }
+
     
+
+    render() {
+
+        return(
+          <div>
+              <img src= {this.state.imagemFood} alt="foto "></img>
+              <div>
+                <button onClick={() => {
+                    this.setState({imagemFood:sushi})
+                }
+                    }>Sushi</button>
+
+                <button onClick={() => {
+                    this.setState({imagemFood:burguer})
+                }
+                    }>Hamburguer</button>
+              </div>
+          </div>  
+        )
+    }
 
 }
 
