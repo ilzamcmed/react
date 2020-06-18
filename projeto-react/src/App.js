9f40c13d70c19d67a0be0e9695b9fc106d8518c3
Ilza <ilzammed2@gmail.com>
2020-06-18 16:02:19 -0300

    ajuste para mostrar exemplo

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
-import Card from './componets/Card/Card'
-// import Input from './componets/Input/Input';
-import SectionName from './componets/SectionName/SectionName'
+import SectionFood from './componets/SectionFood/SectionFood';
+
 
 function App() {
   return (
     <div>
-      <Card titulo="Este é um título" subtitulo="Este é um subtitulo" src="https://media.giphy.com/media/kgThPnMG0b40RMC6gV/giphy.gif" />
-     
-      
-      <Card titulo="Este é o segundo título" subtitulo="Este é o segundo subtitulo" src="https://media.giphy.com/media/Ut8nsZYK1SAp42QKmt/giphy.gif" />
-   
-      <Card titulo="Este é o terceiro título" subtitulo="Este é o terceiro subtitulo" src="https://media.giphy.com/media/SVfm9NpBocbLH1Kyur/giphy.gif" />
-     
-      <SectionName />
-
+        <SectionFood />
     </div>
   );
 }
