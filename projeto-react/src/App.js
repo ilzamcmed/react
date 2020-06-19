a9c00ea070ee9f84271a5eb43531304e3d8ade71
Ilza <ilzammed2@gmail.com>
2020-06-19 15:44:22 -0300

    Ajuste de layout para mostrar componentes

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Contador from './componets/Contador/Contador'
-
+import SectionFood from './componets/SectionFood/SectionFood'
+import Comment from './componets/Comment/Comment';
 
 function App() {
   return (
     <div>
        <Contador />
+       <SectionFood />
+       <Comment />
 
     </div>
   );
